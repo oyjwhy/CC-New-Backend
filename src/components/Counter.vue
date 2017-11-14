03823a04267cb8f6a32c68a1ff6a9b491d4e43e4
oyjhwy <757551893@qq.com>
2017-11-14 20:28:02 +0800

    添加了一个demo

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="increament">+</button>
+    <button @click="decreament">-</button>
+    <p>
+      <span>{{nume}}</span>
+    </p>
+  </div>
+</template>
+
+<<script>
+export default {
+  props: ['nume'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+    increament () {
+      this.nume++
+    },
+    decreament () {
+      this.nume--
+    }
+  }
+}
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,15 +17,43 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <Counter :nume="num"></Counter>
+
+    <button @click="incre">+</button>
+    <button @click="decre">-</button>
+    <p><span>parent:{{num}}</span></p>
+
+    <input v-model="num" placeholder="edit me">
+
+    <p v-if="num >= 10"></br> num 大于等于10 </p>
+    <p v-else></br> num 小于10 </p>
+
+    <transition name="fade">
+      <p v-if="num >= 12"></br> num 大于等于12 </p>
+    </transition>
+
   </div>
 </template>
 
 <script>
+import Counter from './Counter'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      num: 10
+    }
+  },
+  components: {
+    Counter
+  },
+  methods: {
+    incre () {
+      this.num++
+    },
+    decre () {
+      this.num--
     }
   }
 }
@@ -46,5 +74,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .8s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in below version 2.1.8 */ {
+  opacity: 0
 }
 </style>
